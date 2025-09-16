1b1f1ef18295737c173acab7fe7de7c4ca773f04
Nate Sales <nate@natesales.net>
2025-09-16 13:23:33 -0400

    refactor: rename cmd directory

---
+++ cmd/client/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/tinfoilsh/stransport/client"
+	"github.com/tinfoilsh/stransport/identity"
+)
+
+var (
+	serverURL    = flag.String("s", "http://localhost:8080", "server URL")
+	identityFile = flag.String("i", "client_identity.json", "client identity file")
+	verbose      = flag.Bool("v", false, "verbose logging")
+)
+
+func main() {
+	flag.Parse()
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("Verbose logging enabled")
+	}
+
+	clientIdentity, err := identity.FromFile(*identityFile)
+	if err != nil {
+		log.Fatalf("failed to get client identity: %v", err)
+	}
+
+	log.WithFields(log.Fields{
+		"public_key_hex": hex.EncodeToString(clientIdentity.MarshalPublicKey()),
+	}).Info("Client identity")
+
+	secureTransport, err := client.NewTransport(*serverURL, clientIdentity)
+	if err != nil {
+		log.Fatalf("failed to create secure client: %v", err)
+	}
+
+	httpClient := &http.Client{
+		Transport: secureTransport,
+	}
+
+	testSecureEndpoint(httpClient)
+	testStreamEndpoint(httpClient)
+}
+
+func testSecureEndpoint(httpClient *http.Client) {
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/secure", *serverURL), bytes.NewBuffer([]byte("nate")))
+	if err != nil {
+		log.Fatalf("failed to create request: %v", err)
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatalf("failed to make secure request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("failed to read response body: %v", err)
+	}
+
+	log.Infof("Response body: %s", string(body))
+}
+
+func testStreamEndpoint(httpClient *http.Client) {
+	log.Info("Testing streaming endpoint...")
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/stream", *serverURL), nil)
+	if err != nil {
+		log.Fatalf("failed to create request: %v", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatalf("failed to make stream request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	log.Info("Streaming response:")
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := resp.Body.Read(buf)
+		if n > 0 {
+			os.Stdout.Write(buf[:n])
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Errorf("Error reading stream: %v", err)
+			break
+		}
+	}
+}
