7af681dd2130d54935d9e4eab89d192f2b93471b
Tanya Verma <vermatanyax@gmail.com>
2025-12-16 14:54:08 -0800

    feat: remove all references to v1/v2 ehbp

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -5,7 +5,7 @@ const (
 	// This is used by the client to send the encapsulated key to the server.
 	EncapsulatedKeyHeader = "Ehbp-Encapsulated-Key"
 
-	// ResponseNonceHeader contains the random nonce for response key derivation (v2).
+	// ResponseNonceHeader contains the random nonce for response key derivation.
 	// The server generates this and includes it in the response so the client
 	// can derive the same response decryption keys.
 	ResponseNonceHeader = "Ehbp-Response-Nonce"
@@ -18,11 +18,4 @@ const (
 
 	// KeysPath is the well-known path for key distribution
 	KeysPath = "/.well-known/hpke-keys"
-
-	// ClientPublicKeyHeader is DEPRECATED and should not be used.
-	// In v1, this header allowed clients to specify their public key for response
-	// encryption. This was vulnerable to MitM attacks where an attacker could
-	// replace the header with their own key. In v2, response keys are derived
-	// from the request's HPKE context instead.
-	ClientPublicKeyHeader = "Ehbp-Client-Public-Key" // DEPRECATED
 )
